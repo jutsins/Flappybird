5e8ba62d326984b3a5957f7437c109102fc534ea
justin laptop <jutsins@gmail.com>
2019-07-24 15:13:49 +0200

    add more obstacles

--- src/obstacle.js
+++ src/obstacle.js
@@ -14,15 +14,50 @@ var obstacle = (function (width, height, color, x, y) {
 
     var collidesWithPlayer = function() {
 
-    }
-
-    var generateEnemy = function() {
-        var randomNumber = Math.random()
-        
-    }
+    };
 
     return {
         update: update,
         x: x, y: y, width: width, height: height,
     }
+});
+
+var obstacleGenerator = (function() {
+    var generate = function(){
+        var randomNumber = Math.random();
+        if (randomNumber < 0.10){
+            //double bird
+            obstacles.push(
+                new obstacle(20,20,"black", areaWidth, areaHeight - 60),
+                new obstacle(20,20,"black", areaWidth + 25, areaHeight - 100),
+                )
+        }
+        if (randomNumber >= 0.10 && randomNumber < 0.20){
+            //tripple cactus
+            obstacles.push(
+                new obstacle(10,30, "green", areaWidth, areaHeight - 40),
+                new obstacle(10,30,"green", areaWidth + 30, areaHeight- 40),
+                new obstacle(10,30,"green", areaWidth + 60, areaHeight- 40)
+            )
+        }
+        if (randomNumber >= 0.20 && randomNumber < 0.40){
+            //spawn two cacti
+            obstacles.push(
+                new obstacle(10,30, "green", areaWidth, areaHeight - 40),
+                new obstacle(10,30,"green", areaWidth + 35, areaHeight- 40)
+            )
+        }
+        if (randomNumber >= 0.40 && randomNumber < 0.80){
+            //spawn a single cactus
+            obstacles.push(new obstacle(10, 30, "green", areaWidth, areaHeight - 40));
+        }
+        if (randomNumber >= 0.80){
+            //spawn a single bird
+            obstacles.push(new obstacle(20,20,"black", areaWidth, areaHeight - 60));
+        }
+    }
+    return {
+        generate:generate,
+    }
+
 });
